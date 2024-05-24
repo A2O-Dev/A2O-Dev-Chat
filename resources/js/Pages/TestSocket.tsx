b8c44eec1d6231dd505583e0870db9a7212fcdc8
Jdavid <201705961@est.umss.edu>
2024-05-24 11:37:27 -0400

    feature/RI-1523 adding mui and testing whisper channels

--- resources/js/Pages/TestSocket.tsx
+++ resources/js/Pages/TestSocket.tsx
@@ -5,15 +5,6 @@ import Button from '@mui/material/Button'
 const TestSocket: FC = () => {
   useEffect(() => {
     echo.channel('TestChat')
-      // .here((users) => {
-      //   console.log('Users currently in the channel:', users)
-      // })
-      // .joining((user) => {
-      //   console.log('User joined:', user)
-      // })
-      // .leaving((user) => {
-      //   console.log('User left:', user)
-      // })
       .listenForWhisper('TestChatEvent', (e) => {
         console.log('Received whisper:', e)
       })
