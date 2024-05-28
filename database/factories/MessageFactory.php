3adc3ad5cd4e03d6a409b81eb6ec638248e4fd3b
Jdavid <201705961@est.umss.edu>
2024-05-28 16:15:19 -0400

    feature/RI-1605 setting up db  migrations seeders models

---
+++ database/factories/MessageFactory.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Room;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Message>
+ */
+class MessageFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'message' => $this->faker->paragraph,
+            'room_id' => Room::inRandomOrder()->first()->id,
+            'user_id' => User::inRandomOrder()->first()->id,
+        ];
+    }
+}
