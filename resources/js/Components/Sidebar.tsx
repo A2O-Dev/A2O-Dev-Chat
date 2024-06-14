2a23e466f7a3fb24c062138262e2086b8b0cd790
Alvaro Iporre <alvaroiporremartinez@gmail.com>
2024-06-14 15:06:12 -0400

    Add Sidebar improvements

--- resources/js/Components/Sidebar.tsx
+++ resources/js/Components/Sidebar.tsx
@@ -1,8 +1,13 @@
 import InboxIcon from '@mui/icons-material/MoveToInbox'
 import MailIcon from '@mui/icons-material/Mail'
+import PersonIcon from '@mui/icons-material/Person'
 import { ReactElement, FC } from 'react'
 import ApplicationLogo from './ApplicationLogo'
 import NavLinkSidebar from './NavLinkSidebar'
+import { User } from '@/types'
+import Dropdown from './Dropdown'
+import { Divider } from '@mui/material'
+import { Logout } from '@mui/icons-material'
 
 interface MenuItem {
   href: string
@@ -12,21 +17,25 @@ interface MenuItem {
 }
 
 const menuItems: MenuItem[] = [
-  { href: 'chats', text: 'All chats', icon: <InboxIcon />, notifications: 43 },
-  { href: 'dashboard', text: 'News', icon: <MailIcon />, notifications: 0 }
+  { href: 'dashboard', text: 'Dashboard', icon: <MailIcon />, notifications: 0 },
+  { href: 'chat', text: 'Chats', icon: <InboxIcon />, notifications: 43 }
 ]
 
-const Sidebar: FC = () => {
+interface SidebarProps {
+  user: User
+}
+
+const Sidebar: FC<SidebarProps> = ({ user }) => {
   return (
     <div className='w-full h-full text-white'>
       <div className='p-4'>
         <ApplicationLogo />
       </div>
 
-      <div className='w-full text-center'>
+      <div className='w-full text-center flex flex-col gap-5 items-center'>
         {menuItems.map((item, index) => (
           <NavLinkSidebar key={index} href={route(item.href)} active={route().current(item.href)}>
-            <div className='relative flex flex-col justify-center items-center w-full'>
+            <div className='relative flex flex-col justify-center items-center'>
               <p className='text-4xl'>
                 {item.icon}
               </p>
@@ -39,6 +48,24 @@ const Sidebar: FC = () => {
             </div>
           </NavLinkSidebar>
         ))}
+
+        <NavLinkSidebar href={route('profile.edit')} active={route().current('profile.edit')}>
+          <div className='relative flex flex-col justify-center items-center w-full'>
+            <p className='text-4xl'>
+              <PersonIcon />
+            </p>
+            <p>Profile </p>
+          </div>
+        </NavLinkSidebar>
+
+        <NavLinkSidebar href={route('logout')} method='post' as='button' active={route().current('logout')}>
+          <div className='relative flex flex-col justify-center items-center w-full'>
+            <p className='text-4xl'>
+              <Logout />
+            </p>
+            <p>Log Out</p>
+          </div>
+        </NavLinkSidebar>
       </div>
     </div>
   )
