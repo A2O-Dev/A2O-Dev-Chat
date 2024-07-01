66e972a03a674f7e5f537a372746d83f7a38d875
alvaroia2odev <alvaro.iporre@a2odev.com>
2024-07-01 13:59:34 -0400

    feature/RI-1607: Add MessageSent event

---
+++ app/Events/MessageSent.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Events;
+
+use App\Models\Message;
+use Illuminate\Broadcasting\Channel;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Broadcasting\PrivateChannel;
+use Illuminate\Broadcasting\PresenceChannel;
+use Illuminate\Broadcasting\InteractsWithSockets;
+use Illuminate\Contracts\Broadcasting\ShouldBroadcast;
+
+class MessageSent implements ShouldBroadcast
+{
+    use InteractsWithSockets, SerializesModels;
+
+    
+
+    public function __construct(public Message $message)
+    {
+        
+    }
+
+    public function broadcastOn()
+    {
+        return [
+            new Channel('chat')
+        ];
+    }
+}
