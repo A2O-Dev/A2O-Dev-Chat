056afa45f542b024ef107ab6beeeb7bc06d22dc9
alvaroia2odev <alvaro.iporre@a2odev.com>
2024-07-15 13:56:29 -0400

    feature/RI-1705: Add CustomTabPanel component

---
+++ resources/js/Components/CustomTabPanel.tsx
@@ -0,0 +1,26 @@
+import { Box } from "@mui/material";
+
+interface TabPanelProps {
+    children?: React.ReactNode;
+    index: number;
+    value: number;
+  }
+  
+  function CustomTabPanel(props: TabPanelProps) {
+    const { children, value, index, ...other } = props;
+  
+    return (
+      <Box
+        role="tabpanel"
+        hidden={value !== index}
+        id={`simple-tabpanel-${index}`}
+        aria-labelledby={`simple-tab-${index}`}
+        {...other}
+        sx={{ width: '100%'}}
+      >
+        {value === index && <Box sx={{ p: 3, width: '100%' }}>{children}</Box>}
+      </Box>
+    );
+  }
+
+export default CustomTabPanel
