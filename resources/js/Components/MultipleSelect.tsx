4c821910963aea43f09e8225870a863ba6817e05
alvaroia2odev <alvaro.iporre@a2odev.com>
2024-07-15 14:39:34 -0400

    feature/RI-1705: Add MultipleSelect component

---
+++ resources/js/Components/MultipleSelect.tsx
@@ -0,0 +1,76 @@
+import { Theme, useTheme } from '@mui/material/styles';
+import OutlinedInput from '@mui/material/OutlinedInput';
+import InputLabel from '@mui/material/InputLabel';
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select, { SelectChangeEvent } from '@mui/material/Select';
+import { useState } from 'react';
+import { Box } from '@mui/material';
+
+const ITEM_HEIGHT = 48;
+const ITEM_PADDING_TOP = 8;
+const MenuProps = {
+  PaperProps: {
+    style: {
+      maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+      width: 250,
+    },
+  },
+};
+
+
+interface MultipleSelectProps {
+    list: {id: number, value: string}[]
+}
+
+function getStyles(name: string, personName: string[], theme: Theme) {
+  return {
+    fontWeight:
+      personName.indexOf(name) === -1
+        ? theme.typography.fontWeightRegular
+        : theme.typography.fontWeightMedium,
+  };
+}
+
+function MultipleSelect({ list }: MultipleSelectProps) {
+  const theme = useTheme();
+  const [personName, setPersonName] = useState<string[]>([]);
+
+  const handleChange = (event: SelectChangeEvent<typeof personName>) => {
+    const {
+      target: { value },
+    } = event;
+    setPersonName(
+      typeof value === 'string' ? value.split(',') : value
+    );
+  };
+
+  return (
+    <Box sx={{ width: '100%' }}>
+      <FormControl sx={{ width: '100%' }}>
+        <InputLabel id="demo-multiple-name-label">Name</InputLabel>
+        <Select
+          labelId="demo-multiple-name-label"
+          id="demo-multiple-name"
+          multiple
+          value={personName}
+          onChange={handleChange}
+          input={<OutlinedInput label="Name" />}
+          MenuProps={MenuProps}
+        >
+          {list.map((item) => (
+            <MenuItem
+              key={item.id}
+              value={item.id}
+              style={getStyles(item.value, personName, theme)}
+            >
+              {item.value}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </Box>
+  );
+}
+
+export default MultipleSelect
