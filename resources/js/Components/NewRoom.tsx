5a7043c041a28d7ebe724e90ff98e5f4164e48ca
alvaroia2odev <alvaro.iporre@a2odev.com>
2024-08-05 15:45:54 -0400

    feature/RI-1705: Add NewRoom component

---
+++ resources/js/Components/NewRoom.tsx
@@ -0,0 +1,68 @@
+import { NewRoomProps } from "@/interfaces/app";
+import { Box, Button, TextField } from "@mui/material";
+import { FC, useState } from "react";
+import MultipleSelect from "./MultipleSelect";
+
+const NewRoom: FC<NewRoomProps> = ({
+    form,
+    onChange,
+    handleSubmit,
+    users,
+    errors,
+}) => {
+    const [selected, setSelected] = useState<string[]>([]);
+    return (
+        <Box
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                gap: 2,
+            }}
+        >
+            <TextField
+                error={errors?.name !== undefined}
+                label="Room Name"
+                variant="outlined"
+                type="text"
+                name="name"
+                value={form?.name}
+                onChange={onChange}
+                fullWidth
+                helperText={errors?.name}
+            />
+            <MultipleSelect
+                list={users}
+                label="Users"
+                selected={selected}
+                setSelected={setSelected}
+            />
+            <Button
+                variant="outlined"
+                onClick={() => handleSubmit(2, selected, form?.name)}
+                sx={{
+                    bgcolor: "#0049A8",
+                    color: "#fff",
+                    alignSelf: "flex-end",
+                    "&:hover": {
+                        bgcolor: "#055ccf",
+                    },
+                }}
+            >
+                Register
+            </Button>
+            {/*openError && (
+              <Alert
+                onClose={() => setOpenError(false)}
+                severity='error'
+                variant='filled'
+                sx={{ width: '100%' }}
+              >
+                {errors?.name}
+              </Alert>
+            )*/}
+        </Box>
+    );
+};
+
+export default NewRoom;
--- resources/js/interfaces/app.ts
+++ resources/js/interfaces/app.ts
@@ -1,61 +1,78 @@
-import { ChangeEvent } from "react"
+import { ChangeEvent } from "react";
 
 export interface User {
-  created_at: string
-  email: string
-  email_verified_at: string
-  id: number
-  name: string
-  updated_at: string
+    created_at: string;
+    email: string;
+    email_verified_at: string;
+    id: number;
+    name: string;
+    updated_at: string;
 }
 
 export interface Message {
-  created_at: string
-  id: number
-  room_id: number
-  updated_at: string
-  message: string
-  user_id: number
-  user: User
+    created_at: string;
+    id: number;
+    room_id: number;
+    updated_at: string;
+    message: string;
+    user_id: number;
+    user: User;
 }
 
 export interface Room {
-  created_at: string
-  id: number
-  name: string
-  is_direct_message: boolean
-  pivot?: {
-    user_id: number
-    room_id: number
-  }
-  messages?: Message[]
-  updated_at: string
-  notifications?: number
+    created_at: string;
+    id: number;
+    name: string;
+    is_direct_message: boolean;
+    pivot?: {
+        user_id: number;
+        room_id: number;
+    };
+    messages?: Message[];
+    updated_at: string;
+    notifications?: number;
 }
 
 export interface Auth {
-  user: User
+    user: User;
 }
 
 export interface ErrorProps {
-  [key: string]: string | undefined
+    [key: string]: string | undefined;
 }
 
 export interface DashboardProps {
-  users: User[]
-  auth: Auth
-  rooms: Room[]
-  room?: Room
-  messages: Message[]
+    users: User[];
+    auth: Auth;
+    rooms: Room[];
+    room?: Room;
+    messages: Message[];
 }
 
 export interface EmailValidationBody {
-  email: string
+    email: string;
 }
 
 export interface EmailValidationProps {
-  form: EmailValidationBody,
-  onChange: (event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>) => void,
-  handleSubmit: (option: number, list?: string[], name?: string) => void,
-  errors?: ErrorProps
+    form: EmailValidationBody;
+    onChange: (
+        event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => void;
+    handleSubmit: (option: number, list?: string[], name?: string) => void;
+    errors?: ErrorProps;
+}
+
+export interface NewRoomBody {
+    name: string;
+    users?: string[];
+}
+
+export interface NewRoomProps {
+    form: NewRoomBody;
+    onChange: (
+        event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+    ) => void;
+    handleSubmit: (option: number, list?: string[], name?: string) => void;
+    users: User[];
+    errors?: ErrorProps;
 }
